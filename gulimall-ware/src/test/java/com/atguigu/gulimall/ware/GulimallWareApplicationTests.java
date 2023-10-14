2616e6c8c26800db709b6ff63087eec9136e4fa5
liwenfeng16 <liwenfeng16@github.io>
2023-10-14 20:01:17 +0800

    first commit

--- gulimall-ware/src/test/java/com/atguigu/gulimall/ware/GulimallWareApplicationTests.java
+++ gulimall-ware/src/test/java/com/atguigu/gulimall/ware/GulimallWareApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GulimallWareApplicationTests {
+public class GulimallWareApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
